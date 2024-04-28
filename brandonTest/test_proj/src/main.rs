a616dd249667aae5e34a0e8b904cd551e0498425
Brandon Choong <yhchoong01@gmail.com>
2024-04-27 22:36:13 -0500

    Further experimentation with variable declaration and shadowing

--- brandonTest/test_proj/src/main.rs
+++ brandonTest/test_proj/src/main.rs
@@ -18,14 +18,32 @@ fn main() {
     y *= x;
     println!("y is now, after multiplying x: {y}");
 
-    let z = 5;
+    //constants must be declared with type and capitalized
+    const A: i32 = 1;
+    println!("Constant 'A' assigned: {A}");
+
+    //shadowing and variable reassignment
+    //variables can be redecleared when using let.
+    //mutable variable can be redaclared to immutable.
+    let mut z = 5;
+    println!("The value of z is: {z}");
+
+    z += 1;
+    println!("The value of z after increment 1 is: {z}");
 
     let z = z + 1;
 
     {
-        let z = z * 2;
+        //variable still exist in shadowing, however loses the changes happened while shadowing
+        println!("The value of z in the inner scope without redeclaration is: {z}");
+        let mut z = z * 2;
         println!("The value of z in the inner scope is: {z}");
+        z += 1;
+        println!("The value of mutated z in the inner scope is: {z}");
     }
-
     println!("The value of z is: {z}");
+
+    //the following is not allowed after redeclaring variable to immutable.
+    //z -= 1;
+    //println!("The value of z is: {z}");
 }
